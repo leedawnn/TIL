2381018be9ac7b97eabfc78df54c42c635747a98
leedawn <2g_hee@naver.com>
2021-02-14 17:55:48 +0900

    Implement a stack with node

--- python_Algorithms_py/python_Algorithms_7.1.py
+++ python_Algorithms_py/python_Algorithms_7.1.py
@@ -64,6 +64,40 @@ class Stack(object):
         self.head = Node(item, self.head)
         self.count += 1
 
-        
+    def pop(self):
+        if self.count > 0 and self.head:
+            node = self.head
+            self.head = node.pointer
+            self.count -= 1
+            return node.value
+        else:
+            print('Stack is empty.')
+    
+    def peek(self):
+        if self.count > 0 and self.head:
+            return self.head.value
+        else:
+            print('Stack is empty.')
+            
+    def size(self):
+        return self.count
 
-     
+    def _printList(self):
+        node = self.head
+        while node:
+            print(node.value, end=' ')
+            node = node.pointer
+        print()
+
+    if __name__ == '__main__':
+        stack = Stack()
+        print('스택이 비었나요? {0}'.format(stack.isEmpty()))
+        print('스택에 숫자 0~9를 추가합니다.')
+        for i in range(10):
+            stack.push(i)
+        stack._printList()
+        print('스택 크기: {0}'.format(stack.size()))
+        print('peek: {0}'.format(stack.size()))
+        print('pop: {0}'.format(stack.pop()))
+        print('스택이 비었나요? {0}'.format(stack.isEmpty()))
+        stack._printList()
